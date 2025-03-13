98cf37d46ed8bf17853c69d3aa475edd8e4fe39d
neelarani <neela9624@gmail.com>
2025-03-13 21:01:43 +0600

    added trusted section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,16 @@
-
-import { useSelector } from "react-redux"
-import TrustedPartner from "../../components/TrustedPartner/TrustedPartner";
+import { useSelector } from 'react-redux';
+import TrustedPartner from '../../components/Home/TrustedPartner/TrustedPartner';
 
 import React from 'react';
 import FeaturesSectionHomePage from '../../components/Home/FeaturesSection/FeaturesSectionHomePage';
 
-
 const Home = () => {
   return (
     <div>
-
-      Hello i am {user?.displayName} , This is home pages
       <TrustedPartner />
-
       <section>
         <FeaturesSectionHomePage></FeaturesSectionHomePage>
       </section>
-
     </div>
   );
 };

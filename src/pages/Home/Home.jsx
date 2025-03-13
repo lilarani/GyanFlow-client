a51c6bd07959fecc4054df45863a15449d31f8fa
Lila Rani <154080252+lilarani@users.noreply.github.com>
2025-03-13 17:26:13 +0600

    Revert "home page features section add"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import FeaturesSectionHomePage from '../../components/FeaturesSectionHomePage';
 
 const Home = () => {
-  return <div>
-
-    <section>
-<FeaturesSectionHomePage></FeaturesSectionHomePage>
-    </section>
-  </div>;
+  return <div></div>;
 };
 
 export default Home;

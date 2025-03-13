d5e42651b8d7c86e93678507e5faad63f7f76415
Ankon Chowdhury <ankonchy1@gmail.com>
2025-03-13 20:28:18 +0600

    impact at a glance section completed in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import FeaturesSectionHomePage from '../../components/FeaturesSectionHomePage';
+import React from "react";
+import FeaturesSectionHomePage from "../../components/FeaturesSectionHomePage";
+import Impact from "../../components/Home/Impact/Impact";
 
 const Home = () => {
   return (
     <div>
       <section>
         <FeaturesSectionHomePage></FeaturesSectionHomePage>
+
+        {/* Impact at a glance  */}
+        <Impact />
       </section>
     </div>
   );

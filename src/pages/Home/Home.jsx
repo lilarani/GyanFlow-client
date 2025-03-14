ea733b9e3aa5a110d7560ba4fef60a8e41993bd1
neelarani <neela9624@gmail.com>
2025-03-14 22:41:48 +0600

    added about page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,7 @@ import { useSelector } from 'react-redux';
 import TrustedPartner from '../../components/Home/TrustedPartner/TrustedPartner';
 import Impact from '../../components/Home/Impact/Impact';
 import FeaturesSectionHomePage from '../../components/Home/FeaturesSection/FeaturesSectionHomePage';
-import Team from '../../components/About/Team/Team';
-
 import Banner from '../../components/Home/banner/Banner';
-import OurMission from '../../components/OurMission/OurMission';
-
-import About from '../../components/Home/about/About';
 
 const Home = () => {
   return (

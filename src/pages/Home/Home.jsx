5361fa618a007c15fb307b2a7aa80e12785a0b57
neelarani <neela9624@gmail.com>
2025-03-14 08:32:56 +0600

    added banner section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,24 @@
-
-
-import { useSelector } from "react-redux"
-import Banner from "../../components/banner/Banner";
+import { useSelector } from 'react-redux';
 
 import TrustedPartner from '../../components/Home/TrustedPartner/TrustedPartner';
 import Impact from '../../components/Home/Impact/Impact';
 import FeaturesSectionHomePage from '../../components/Home/FeaturesSection/FeaturesSectionHomePage';
-
-
+import Banner from '../../components/Home/banner/Banner';
 
 const Home = () => {
   return (
     <div>
-
-     <Banner/>
+      {/* banner section */}
+      <Banner />
 
       <section>
+        {/* features section */}
         <FeaturesSectionHomePage></FeaturesSectionHomePage>
         {/* Trasted Features */}
         <TrustedPartner />
         {/* Impact at a glance  */}
         <Impact />
       </section>
-
     </div>
   );
 };

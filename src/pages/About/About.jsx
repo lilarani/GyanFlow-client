12b42d565ed74cb580268aa4740057b0e72c8a07
Ankon Chowdhury <ankonchy1@gmail.com>
2025-03-15 22:35:18 +0600

    doughnut,bar,line chart added in admin dashboard using chartjs successfully

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import OurAchievementSection from '../../components/About/OurAchievementSection/OurAchievementSection';
-import Team from './../../components/About/Team/Team';
-import OurMission from '../../components/About/OurMission/OurMission';
+import React from "react";
+import OurAchievementSection from "../../components/About/OurAchievementSection/OurAchievementSection";
+import Team from "./../../components/About/Team/Team";
+import OurMission from "../../components/About/OurMission/OurMission";
+import Statistics from "../../components/Dashboard/Statistics/Statistics";
 
 export default function About() {
   return (

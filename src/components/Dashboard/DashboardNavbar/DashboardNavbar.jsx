b0f7dfcb33102b6c932e8ac31861ec1aec8ab6fc
neelarani <neela9624@gmail.com>
2025-03-15 13:50:28 +0600

    Added Nav Routes to Sidebar for better navigation

--- src/components/Dashboard/DashboardNavbar/DashboardNavbar.jsx
+++ src/components/Dashboard/DashboardNavbar/DashboardNavbar.jsx
@@ -3,6 +3,8 @@ import ukFlagImg from '../../../assets/images/dashboardNavImg/uk.png';
 import chinaFlagImg from '../../../assets/images/dashboardNavImg/china.png';
 import turkeyFlagImg from '../../../assets/images/dashboardNavImg/turkey.png';
 import bdFlagImg from '../../../assets/images/dashboardNavImg/bangladesh.png';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { TbHelpSquare } from 'react-icons/tb';
 
 const flags = [
   { img: usaFlagImg, name: 'English(US)', code: 'en-Us' },
@@ -15,6 +17,7 @@ const flags = [
 import { MdOutlineNotificationsNone } from 'react-icons/md';
 import { useState } from 'react';
 import { FaRegUser } from 'react-icons/fa';
+import { Link } from 'react-router';
 const DashboardNavbar = () => {
   const [openFlagModal, setOpenFlagModal] = useState(false);
   const [selectedFlag, setSelectedFlag] = useState(flags[0]);
@@ -77,12 +80,37 @@ const DashboardNavbar = () => {
             className="w-12 h-12 rounded-full"
           />
           {openUserModal && (
-            <div className="absolute top-18 bg-gradient-to-bl to-[#0b0221] from-[#080127] text-white w-60 h-56  right-4 space-y-6 p-6">
-              <div className="text-white">
-                <p className="text-white">
+            <div className="absolute top-18 bg-gradient-to-bl to-[#0b0221] from-[#080127] text-white w-60 h-56  right-4  p-6">
+              <div className="flex gap-2 items-center border-b-[1px] border-gray-700 p-2">
+                <img src={''} alt="User image" />
+                <div>
+                  <p>name</p>
+                  <p>email</p>
+                </div>
+              </div>
+
+              <div className="text-white space-y-5 mt-4">
+                <Link
+                  to={'#'}
+                  className="text-base font-bold flex gap-2 items-center cursor-pointer"
+                >
                   <FaRegUser />
                   View Profile
-                </p>
+                </Link>
+                <Link
+                  to={'#'}
+                  className="text-base font-bold flex gap-2 items-center cursor-pointer"
+                >
+                  <IoMdNotificationsOutline />
+                  Notification
+                </Link>
+                <Link
+                  to={'#'}
+                  className="text-base font-bold flex gap-2 items-center cursor-pointer"
+                >
+                  <TbHelpSquare />
+                  Help Center
+                </Link>
               </div>
             </div>
           )}
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,7 @@
 import { IoSearch } from 'react-icons/io5';
+import { BiSolidUpArrow } from 'react-icons/bi';
+import { FaDollarSign, FaRegStar, FaRegUser } from 'react-icons/fa';
+import { Link } from 'react-router';
 
 const Sidebar = () => {
   return (
@@ -6,13 +9,45 @@ const Sidebar = () => {
       <h2 className="font-bold text-2xl ">
         <span className="text-yellow-300">G</span>yanFlow
       </h2>
-      <div className="relative flex items-center space-y-3 mt-7">
+      <div className="relative flex items-center space-y-3 mt-10">
         <IoSearch className="absolute left-2 top-2 " />
         <input
           type="text"
           placeholder="Search for..."
           className="text-base pl-7 py-1  border-[1px] border-gray-500 focus:outline-none focus:ring-blue-[#1a044d] focus:ring rounded-lg"
         />
+      </div>
+
+      {/*  */}
+      <div className="space-y-5 mt-10">
+        <Link
+          to={'#'}
+          className="text-lg font-bold flex gap-2 items-center cursor-pointer"
+        >
+          <BiSolidUpArrow />
+          Dashboard
+        </Link>
+        <Link
+          to={'#'}
+          className="text-lg font-bold flex gap-2 items-center cursor-pointer"
+        >
+          <FaRegStar />
+          Features
+        </Link>
+        <Link
+          to={'#'}
+          className="text-lg font-bold flex gap-2 items-center cursor-pointer"
+        >
+          <FaRegUser />
+          Users
+        </Link>
+        <Link
+          to={'#'}
+          className="text-lg font-bold flex gap-2 items-center cursor-pointer"
+        >
+          <FaDollarSign />
+          Pricing
+        </Link>
       </div>
     </div>
   );

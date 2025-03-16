15f6a67aad3e2852d88dd3280cc209b8f55ba958
kawsar firoz <kawsarfiroz11@gmail.com>
2025-03-16 14:53:03 +0300

    added about us banner design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import TrustedPartner from '../../components/Home/TrustedPartner/TrustedPartner'
 import Impact from '../../components/Home/Impact/Impact';
 import FeaturesSectionHomePage from '../../components/Home/FeaturesSection/FeaturesSectionHomePage';
 import Banner from '../../components/Home/banner/Banner';
-import About from '../../components/about/About';
+// import About from '../../components/about/About';
 
 const Home = () => {
   return (
@@ -17,7 +17,7 @@ const Home = () => {
         <FeaturesSectionHomePage></FeaturesSectionHomePage>
         {/* Trasted Features */}
         <TrustedPartner />
-        <About/>
+        {/* <About/> */}
         {/* Impact at a glance  */}
         <Impact />
       </section>

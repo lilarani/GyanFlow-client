8ce7d640dd3da801b9daaf259731718f55b0e0eb
neelarani <neela9624@gmail.com>
2025-03-19 00:50:11 +0600

    added course page

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { IoSearch } from 'react-icons/io5';
 import { BiSolidUpArrow } from 'react-icons/bi';
 import { FaDollarSign, FaRegStar, FaRegUser } from 'react-icons/fa';
 import { Link } from 'react-router';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { useState } from 'react';
-=======
-import { IoSearch } from "react-icons/io5";
-import { BiSolidUpArrow } from "react-icons/bi";
-import { FaDollarSign, FaRegStar, FaRegUser } from "react-icons/fa";
-import { Link } from "react-router";
->>>>>>> fb8fd609446d23230c2b036fa32b5e73ef9a6abc
 
 const Sidebar = () => {
   // let adminRole = 'admin';
@@ -18,19 +11,14 @@ const Sidebar = () => {
   // let instructor = ' instructor';
   const [isOpen, setIsOpen] = useState(false);
 
-  const role = "admin";
+  const role = 'admin';
   return (
     <div className="w-full min-h-screen bg-gradient-to-bl to-[#1a044d] from-[#080127] text-white p-2 pt-8 md:p-8">
-<<<<<<< HEAD
       <Link
         to={'/'}
         className="font-bold text-base md:text-2xl hidden md:block block"
       >
         <span className="text-yellow-300 ">G</span>yanFlow
-=======
-      <Link to={"/"} className="font-bold text-base md:text-2xl ">
-        <span className="text-yellow-300">G</span>yanFlow
->>>>>>> fb8fd609446d23230c2b036fa32b5e73ef9a6abc
       </Link>
       <GiHamburgerMenu className="text-white block md:hidden" />
       <div className="relative flex items-center space-y-3 mt-10">
@@ -43,24 +31,24 @@ const Sidebar = () => {
       </div>
 
       {/* admin role */}
-      {role === "admin" && (
+      {role === 'admin' && (
         <div className="space-y-5 mt-10">
           <Link
-            to={"/dashboard/adminDashBoard"}
+            to={'/dashboard/adminDashBoard'}
             className="text-sm md:text-lg font-bold flex gap-2 items-center cursor-pointer hover:bg-[#ffffff44] md:px-4 py-2"
           >
             <BiSolidUpArrow />
             Dashboard
           </Link>
           <Link
-            to={"/dashboard/courses"}
+            to={'/dashboard/courses'}
             className="text-sm md:text-lg font-bold flex gap-2 items-center cursor-pointer hover:bg-[#ffffff44] md:px-4 py-2"
           >
             <FaRegStar />
             Courses
           </Link>
           <Link
-            to={"/dashboard/features"}
+            to={'/dashboard/features'}
             className="text-sm md:text-lg font-bold flex gap-2 items-center cursor-pointer hover:bg-[#ffffff44] md:px-4 py-2"
           >
             <FaRegStar />
@@ -68,14 +56,14 @@ const Sidebar = () => {
           </Link>
 
           <Link
-            to={"#"}
+            to={'#'}
             className="text-sm md:text-lg font-bold flex gap-2 items-center cursor-pointer hover:bg-[#ffffff44] md:px-4 py-2"
           >
             <FaRegUser />
             Users Managment
           </Link>
           <Link
-            to={"#"}
+            to={'#'}
             className="text-sm md:text-lg font-bold flex gap-2 items-center cursor-pointer hover:bg-[#ffffff44] md:px-4 py-2"
           >
             <FaDollarSign />
@@ -84,7 +72,7 @@ const Sidebar = () => {
 
           <div className="w-full mt-4  border-gray-600 border-t-[1px]">
             <Link
-              to={"/"}
+              to={'/'}
               className="cursor-pointer block w-full text-base md:text-xl font-bold md:mt-8 hover:bg-[#ffffff44] px-4 py-2"
             >
               Home
@@ -93,10 +81,10 @@ const Sidebar = () => {
         </div>
       )}
       {/* student role */}
-      {role === "student" && (
+      {role === 'student' && (
         <div className="space-y-5 mt-10">
           <Link
-            to={"/dashboard/studentDashboard"}
+            to={'/dashboard/studentDashboard'}
             className="text-lg font-bold flex gap-2 items-center cursor-pointer"
           >
             <BiSolidUpArrow />
@@ -106,10 +94,10 @@ const Sidebar = () => {
       )}
 
       {/* Instructor role */}
-      {role === "instructor" && (
+      {role === 'instructor' && (
         <div className="space-y-5 mt-10">
           <Link
-            to={"/dashboard/instructorDasboard"}
+            to={'/dashboard/instructorDasboard'}
             className="text-lg font-bold flex gap-2 items-center cursor-pointer"
           >
             <BiSolidUpArrow />
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createBrowserRouter } from 'react-router';
 import Main from '../layouts/MainLayout/Main';
 import Home from '../pages/Home/Home';
@@ -12,58 +11,38 @@ import Features from '@/pages/Dashboard/AdminDashboard/Features/Features';
 import StudentDashboard from '@/pages/Dashboard/StudentDashboard/StudentDashboard';
 import InstructorDash from '@/pages/Dashboard/InstructorDashboard/InstructorDash/InstructorDash';
 import UserProfile from '@/components/Dashboard/UserProfile/UserProfile';
-=======
-import { createBrowserRouter } from "react-router";
-import Main from "../layouts/MainLayout/Main";
-import Home from "../pages/Home/Home";
-import Login from "../authentication/Login/Login";
-import Register from "../authentication/Register/Register";
-import Support from "../pages/Support/Support";
-import About from "../pages/About/About";
-import DashboardPage from "../pages/Dashboard/DashboardPage/DashboardPage";
-import AdminDash from "@/layouts/DashboardLayout/AdminDashboard/AdminDash/AdminDash";
-import Features from "@/layouts/DashboardLayout/AdminDashboard/Features/Features";
-// import StudentDash from '@/layouts/DashboardLayout/StudentDashboard/StudentDash/StudentDash';
-import InstructorDash from "@/layouts/DashboardLayout/InstructorDashboard/InstructorDash/InstructorDash";
-import StudentDashboard from "@/DashboardLayout/StudentDashboard/StudentDashboard";
-import Courses from "@/pages/Dashboard/AdminDashboard/Courses/Courses";
->>>>>>> fb8fd609446d23230c2b036fa32b5e73ef9a6abc
+import Courses from '@/pages/Dashboard/AdminDashboard/Courses/Courses';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Main></Main>,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home></Home>,
       },
       {
-        path: "/about",
+        path: '/about',
         element: <About></About>,
       },
       {
-        path: "/support",
+        path: '/support',
         element: <Support></Support>,
       },
     ],
   },
   {
-    path: "/login",
+    path: '/login',
     element: <Login></Login>,
   },
   {
-    path: "/register/:role",
+    path: '/register/:role',
     element: <Register></Register>,
   },
   {
-<<<<<<< HEAD
     path: '/dashboard',
     element: <Dashboard></Dashboard>,
-=======
-    path: "/dashboard",
-    element: <DashboardPage></DashboardPage>,
->>>>>>> fb8fd609446d23230c2b036fa32b5e73ef9a6abc
     children: [
       // admin related routes
       {
@@ -71,29 +50,24 @@ export const router = createBrowserRouter([
         element: <AdminDashboard></AdminDashboard>,
       },
       {
-<<<<<<< HEAD
         path: 'adminDashBoard',
         element: <AdminDashboard></AdminDashboard>,
-=======
-        path: "adminDashBoard",
-        element: <AdminDash></AdminDash>,
->>>>>>> fb8fd609446d23230c2b036fa32b5e73ef9a6abc
       },
       {
-        path: "features",
+        path: 'features',
         element: <Features></Features>,
       },
       {
-        path: "courses",
+        path: 'courses',
         element: <Courses />,
       },
       // student related  routes
       {
-        path: "studentDashboard",
+        path: 'studentDashboard',
         element: <StudentDashboard></StudentDashboard>,
       },
       {
-        path: "instructorDasboard",
+        path: 'instructorDasboard',
         element: <InstructorDash></InstructorDash>,
       },
     ],
